84aa5afa24ed17550912e47e415b75961521828a
Omii Mughal <import>
2022-11-06 18:36:36 +0500

    app internationalized

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:asignment/screens/splash_screen.dart';
 import 'package:asignment/services/i18n.dart';
+import 'package:asignment/utils/shared_pref.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+ await PreferencesService.initializeStorage();
   runApp(const MyApp());
 }
 
--- lib/widgets/full_width_button.dart
+++ lib/widgets/full_width_button.dart
@@ -1,29 +1,34 @@
-
+import 'package:asignment/controllers/lang_controller.dart';
 import 'package:asignment/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 class FullWidthButton extends StatelessWidget {
-   FullWidthButton({
-    Key? key,
-    required this.buttonName,
-    required this.onPressed
-  }) : super(key: key);
-void Function()? onPressed;
-String buttonName;
+  FullWidthButton({Key? key, required this.buttonName, required this.onPressed})
+      : super(key: key);
+  void Function()? onPressed;
+  String buttonName;
+
+  LanguageController languageController = Get.find();
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-          onPressed: onPressed,
-          style: ElevatedButton.styleFrom(
-         backgroundColor: AppColor.scondary,
-         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.r)
-         ),
-        
-         fixedSize: Size(MediaQuery.of(context).size.width, 56.sp)
-          ),
-          child: Text(buttonName, style: TextStyle(color: Colors.white, fontFamily: "Poppins-Regular", fontSize: 18.sp),),
-        );
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: AppColor.scondary,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+          fixedSize: Size(MediaQuery.of(context).size.width, 56.sp)),
+      child: Text(
+        buttonName,
+        style: TextStyle(
+            color: Colors.white,
+            fontFamily: languageController.isEnglish.value
+                ? "Poppins-Semibold"
+                : "NotoKufiArabic-Bold",
+            fontSize: 18.sp),
+      ),
+    );
   }
 }
